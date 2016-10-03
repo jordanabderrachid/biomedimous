92aea9ba0411f036fbed93227200f725e1161ac5
Jordan Abderrachid <jabderrachid@gmail.com>
2016-10-03 17:27:12 +0200

    Remove remaining js files.

--- src/routes/api.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var express_1 = require('express');
-var auth_1 = require('./auth');
-exports.apiRoutes = express_1.Router()
-    .use('/auth', auth_1.authRoutes);
--- src/routes/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var express_1 = require('express');
-var authController = require('../controllers/auth');
-exports.authRoutes = express_1.Router()
-    .put('/', authController.createUser);
